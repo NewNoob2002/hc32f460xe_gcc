e51244ccc2b0942e9287ef08518bb6aadc81d119
NewNoob2002 <guptianci@gmail.com>
2025-07-10 09:33:20 +0800

    delete screen

--- user/main.cpp
+++ user/main.cpp
@@ -1,17 +1,10 @@
 #include <Arduino.h>
 #include "init/init.h"
-#include "LCD.h"
-#include "MYSPI.h"
 
 int main()
 {
     core_init();
     pinMode(PC13, OUTPUT);
-
-    Spi_Config();
-    Spi_DmaConfig();
-    LCD_Simple_inition();
-    showpic_a();
 
     while (true) {
         digitalToggle(PC13);
